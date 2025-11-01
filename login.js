e6ea3c8a44a67b26c867895c99d9a6071d44d72e
cassiusadeoliveira-crypto <cassiusadeoliveira@gmail.com>
2025-11-01 07:39:31 -0300

    Update login.js

--- login.js
+++ login.js
@@ -17,7 +17,7 @@ document.getElementById('googleLoginBtn').addEventListener('click', async () => 
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: window.location.origin + '/crm-astro/index.html'
+            redirectTo: 'https://cassiusadeoliveira-crypto.github.io/crm-astro/index.html'
         }
     });
     
